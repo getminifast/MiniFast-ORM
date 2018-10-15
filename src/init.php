7d04daf00c302966953d34812c6c734f96593b30
getminifast <contact@getminifast.org>
2018-10-15 22:27:37 +0200

    PSR-2 update

--- src/init.php
+++ src/init.php
@@ -9,16 +9,12 @@ function printUsage()
     echo "Usage: init.php <xml_file>" . PHP_EOL;
 }
 
-if(sizeof($argv) > 1)
-{
-    if(file_exists($argv[1]))
-    {
+if (sizeof($argv) > 1) {
+    if (file_exists($argv[1])) {
         $database = new Database($argv[1]);
         $database->createSQL();
         $database->createClasses();
     }
-}
-else
-{
+} else {
     printUsage();
 }
